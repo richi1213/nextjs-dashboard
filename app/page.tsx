1689a8581d2fed15433644a3aab771cb4b70d008
richi1213 <datonanava11@gmail.com>
2024-12-29 23:12:21 +0400

    completed chapter 16

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@ import { lusitana } from '@/app/ui/fonts';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default function Page() {
   return (
